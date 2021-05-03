dea1a08b2e75887f3c86dbe091183a3073b0c695
Quinn Bigane <51971293+QuinnBigane@users.noreply.github.com>
2021-05-03 12:23:01 -0700

    Inital fuzz tester setup

---
+++ ABA_fuzzer.py
@@ -0,0 +1,7 @@
+class Address_Book_Fuzzer:
+    def __init__(self):
+        self.test_run()
+    def test_run(self):
+        print("LIN admin")
+if __name__ == "__main__":
+    Address_Book_Fuzzer()
